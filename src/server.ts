36ed70363b5fdb9654bd8909c35bbad01d1c5f45
thekingofjibs <26507833+thekingofjibs@users.noreply.github.com>
2020-11-11 01:28:29 +0000

    context includes user info

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,48 @@
 import express from "express";
+var cookieParser = require("cookie-parser");
 import { ApolloServer } from "apollo-server-express";
 import depthLimit from "graphql-depth-limit";
 import { createServer } from "http";
 import compression from "compression";
 import cors from "cors";
+import jwt from "jsonwebtoken";
+
 import schema from "./schema";
 import { db } from "./resolverMap";
 const app = express();
 const server = new ApolloServer({
   schema,
   validationRules: [depthLimit(10)],
+  context: ({ req }) => {
+    // Note! This example uses the `req` object to access headers,
+    // but the arguments received by `context` vary by integration.
+    // This means they will vary for Express, Koa, Lambda, etc.!
+    //
+    // To find out the correct arguments for a specific integration,
+    // see the `context` option in the API reference for `apollo-server`:
+    // https://www.apollographql.com/docs/apollo-server/api/apollo-server/
+
+    // Get the user token from the headers.
+    //const token = req.headers.authorization || '';
+
+    // try to retrieve a user with the token
+    //const user = getUser(token);
+
+    // add the user to the context
+    //return { user };
+
+    const secret: jwt.Secret = process.env.signing_key!;
+
+    try {
+      var decoded = jwt.verify(req.cookies.token, secret);
+      return decoded;
+    } catch (error) {
+      return null;
+    }
+  },
 });
 app.use("*", cors());
+app.use(cookieParser());
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 server.applyMiddleware({ app, path: "/graphql" });
